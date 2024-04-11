3fa3ac9f17d44b23e716dcc11d28b49c11219d0e
tais <tais@Taissa>
2024-04-10 22:12:39 -0300

    commit

--- Sobrecarga/src/Sobrecarga/Calculadora.java
+++ Sobrecarga/src/Sobrecarga/Calculadora.java
@@ -33,7 +33,39 @@ public class Calculadora {
 		
 		switch (tipo) {
 		//passagem de parametros retornando por função
-		case 1 : System.out.println("Adição 2 valores inteiros="+operaçao.soma(valor1, valor2)); break;
+		case 1 : System.out.println("Adição 2 valores inteiros="+operacao.soma(valor1, valor2)); break;
+		//passagem por método e retornando por função
+		case 2 : operacao.setMinuendo(valor1);
+		         operacao.setSubtraendo(valor2);
+		         System.out.println("Subtração="+operacao.subtracao());
+		         break;
+		case 3 : operacao.setNumeroGenerico1(valor1); 
+		         operacao.setNumeroGenerico2(valor2);
+		         System.out.println("Multiplicação="+(valor1*valor2));
+		         break;
+		case 4 : operacao.setNumeroGenerico1(valor1);
+		         operacao.setNumeroGenerico2(valor2);
+		         System.out.println("Divisão="+(valor1/valor2));
+		         break;
+		case 5 : double base = valor1; 
+	    // Substitua pelo valor da base
+        		 double expoente = valor2; 
+        // Substitua pelo valor do expoente
+
+        // Calculando a exponenciação usando Math.pow
+        		double resultadoExponenciacao = Math.pow(base, expoente);
+
+        		System.out.println("Resultado da exponenciação: " + resultadoExponenciacao);
+        		break;
+		//passagem por método e retornanado por função
+		case 6 : operacao.setFatorial(valor1);
+		         System.out.println("Fatorial="+operacao.getFatorial());
+		         break;
+		//sobrecarga soma
+		case 7 : System.out.println("adição 3 valores="+operacao.soma(valor1,valor2,78)); break;
+		case 8 : System.out.println("adição 2 valores textos="+operacao.soma(Integer.toString(valor1), Integer.toString(valor2))); break;
+		default : System.out.println("Operação inválida!");
+		
 		}
 	}
 
